0ec19ec41ca30cbd8b297a0c04b9a1a44fb9b2f8
Frank Campanelli <frcampanelli412@gmail.com>
2019-03-29 15:38:48 -0400

    weaponized refactoring

--- src/simpledb/buffer/AdvancedBufferMgr.java
+++ src/simpledb/buffer/AdvancedBufferMgr.java
@@ -1,6 +1,7 @@
 package simpledb.buffer;
 
 import simpledb.file.*;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 
@@ -10,25 +11,24 @@ public class AdvancedBufferMgr {
     private int numAvailable = 0;
     private ArrayList<Integer> freeList;
     private int nextBuff = 0;
-    private HashMap<Block,Integer> BufferHash;
+    private HashMap<Block, Integer> BufferHash;
 
 
-
-    AdvancedBufferMgr(int numBuffs){
+    AdvancedBufferMgr(int numBuffs) {
         bufferPool = new Buffer[numBuffs];
         numAvailable = numBuffs;
         BufferHash = new HashMap<>(numBuffs);
         freeList = new ArrayList<>();
 
         //loop through the number of buffers that there will be, add them to the buffer pool then add them to the free list
-        for(int i = 0; i < numBuffs; i++){
+        for (int i = 0; i < numBuffs; i++) {
             bufferPool[i] = new Buffer();
             freeList.add(i);
             numAvailable++;
         }
     }
 
-     void pin(Block blk){
+    void pin(Block blk) {
         //find an unpinned buffer
         //put the block into the buffer hashmap
         int unpinnedBuffNum = ChooseUnpinnedBuff();
@@ -38,36 +38,33 @@ public class AdvancedBufferMgr {
         //pin the unpinned buffer
         bufferPool[unpinnedBuffNum].pin();
         //add the block to the buffer
-         bufferPool[unpinnedBuffNum].assignToBlock(blk);
+        bufferPool[unpinnedBuffNum].assignToBlock(blk);
         //remove the free bufferNumber from the list
         freeList.remove(0);
         numAvailable--;
     }
 
-    void unpin(Buffer buff){
+    void unpin(Buffer buff) {
         buff.unpin();
-        if(!buff.isPinned()){
+        if (!buff.isPinned()) {
             BufferHash.remove(buff.block());
             numAvailable++;
         }
     }
 
-    int findBlock(Block blk){
-        if(BufferHash.containsKey(blk)){
+    int findBlock(Block blk) {
+        if (BufferHash.containsKey(blk)) {
             return BufferHash.get(blk);
-        }
-        else{
+        } else {
             return 0;
         }
     }
 
-    int ChooseUnpinnedBuff(){
+    int ChooseUnpinnedBuff() {
 
-        if(freeList.isEmpty()){
+        if (freeList.isEmpty()) {
 
-        }
-
-        else{
+        } else {
             return freeList.get(0);
         }
 
--- src/simpledb/planner/QueryPlanner.java
+++ src/simpledb/planner/QueryPlanner.java
@@ -5,18 +5,19 @@ import simpledb.query.Plan;
 import simpledb.parse.QueryData;
 
 /**
- * The interface implemented by planners for 
+ * The interface implemented by planners for
  * the SQL select statement.
- * @author Edward Sciore
  *
+ * @author Edward Sciore
  */
 public interface QueryPlanner {
-   
-   /**
-    * Creates a plan for the parsed query.
-    * @param data the parsed representation of the query
-    * @param tx the calling transaction
-    * @return a plan for that query
-    */
-   public Plan createPlan(QueryData data, Transaction tx);
+
+    /**
+     * Creates a plan for the parsed query.
+     *
+     * @param data the parsed representation of the query
+     * @param tx   the calling transaction
+     * @return a plan for that query
+     */
+    public Plan createPlan(QueryData data, Transaction tx);
 }
